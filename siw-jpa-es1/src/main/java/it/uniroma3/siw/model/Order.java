ce24f4d8d5100b03789895bb0a73bfc613aebc3b
Chiodoo <marco.chiodo03@gmail.com>
2025-03-31 09:51:46 +0200

    Modificata strategia di fetch delle OrderLine di Order

--- siw-jpa-es1/src/main/java/it/uniroma3/siw/model/Order.java
+++ siw-jpa-es1/src/main/java/it/uniroma3/siw/model/Order.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,7 +25,7 @@ public class Order {
     @Column(nullable = false)
     private LocalDateTime creationTime;
 
-    @OneToMany
+    @OneToMany(fetch=FetchType.EAGER)
     @JoinColumn(name = "order_id")
     private List<OrderLine> orderLines;
 
