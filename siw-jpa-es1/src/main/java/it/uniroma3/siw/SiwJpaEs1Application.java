ed94443aa80a37b711fb87cb543f1d8376af3fe8
Chiodoo <marco.chiodo03@gmail.com>
2025-04-01 08:49:24 +0200

    Esercitazione es2 layout iniziale

--- siw-jpa-es1/src/main/java/it/uniroma3/siw/SiwJpaEs1Application.java
+++ siw-jpa-es1/src/main/java/it/uniroma3/siw/SiwJpaEs1Application.java
@@ -1,13 +1,23 @@
 package it.uniroma3.siw;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.transaction.Transactional;
+
+CustomerRepository customerRepository;
+
 @SpringBootApplication
-public class SiwJpaEs1Application {
+public class SiwJpaEs1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiwJpaEs1Application.class, args);
+	}
+
+	@Transactional
+	public void run(String... args) throws Exception {
 	}
 
 }
