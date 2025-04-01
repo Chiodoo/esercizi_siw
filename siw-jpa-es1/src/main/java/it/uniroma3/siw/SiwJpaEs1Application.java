5ec91101a61f69ad061b3e8986ab2182b8076b3c
Chiodoo <marco.chiodo03@gmail.com>
2025-04-01 09:32:09 +0200

    Aggiunto address e verificato il salvataggio dell'address attraverso il salvataggio del customer attraverso gli eventi a cascata

--- siw-jpa-es1/src/main/java/it/uniroma3/siw/SiwJpaEs1Application.java
+++ siw-jpa-es1/src/main/java/it/uniroma3/siw/SiwJpaEs1Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.uniroma3.siw.model.Address;
 import it.uniroma3.siw.model.Customer;
-import it.uniroma3.siw.repository.AddressRepository;
 import it.uniroma3.siw.repository.CustomerRepository;
 import jakarta.transaction.Transactional;
 
@@ -16,9 +16,6 @@ public class SiwJpaEs1Application implements CommandLineRunner {
 	
 	@Autowired
 	private CustomerRepository customerRepository;
-
-	@Autowired
-	private AddressRepository addressRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiwJpaEs1Application.class, args);
@@ -32,14 +29,28 @@ public class SiwJpaEs1Application implements CommandLineRunner {
 		customer.setFirstName("Harold");
 		customer.setLastName("Barroga");
 
+		//Creo un nuovo address
+		Address address = new Address();
+		address.setStreet("Via della vasca navale");
+		address.setCity("Roma");
+		address.setCountry("Italia");
+		address.setZipcode("00146");
+
+		//Associo l'indirizzo al customer
+		customer.setAddress(address);
+
+
+		System.out.println("========================================================================================================================");
 		//Stampo le informazioni sul customer
 		System.out.println("Customer prima di save:\n"+customer);
+		System.out.println("Address prima di save:\n"+address);
 
 		//Salvo il customer nel database
 		customerRepository.save(customer);
 
 		//Stampo le informazioni sul customer
 		System.out.println("Customer dopo save:\n"+customer);
+		System.out.println("Address dopo di save:\n"+address);
 	}
 
 }
--- siw-jpa-es1/src/main/java/it/uniroma3/siw/model/Address.java
+++ siw-jpa-es1/src/main/java/it/uniroma3/siw/model/Address.java
@@ -11,7 +11,7 @@ public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String street;
