9014b1bb75c96517ed7e1cf926360031875d688a
Chiodoo <marco.chiodo03@gmail.com>
2025-04-01 08:50:46 +0200

    Secondo commit, primo not working

--- siw-jpa-es1/src/main/java/it/uniroma3/siw/SiwJpaEs1Application.java
+++ siw-jpa-es1/src/main/java/it/uniroma3/siw/SiwJpaEs1Application.java
@@ -5,12 +5,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.uniroma3.siw.repository.AddressRepository;
+import it.uniroma3.siw.repository.CustomerRepository;
 import jakarta.transaction.Transactional;
 
-CustomerRepository customerRepository;
 
 @SpringBootApplication
 public class SiwJpaEs1Application implements CommandLineRunner {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@Autowired
+	private AddressRepository addressRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiwJpaEs1Application.class, args);
