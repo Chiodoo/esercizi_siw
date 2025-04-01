c6b6f9fd66a8dc0f21849be7b56f155c75669f9d
Chiodoo <marco.chiodo03@gmail.com>
2025-04-01 08:36:59 +0200

    Aggiunte repository (CrudRepository) per Address, Customer, OrderLine, Order, Provider

---
+++ siw-jpa-es1/src/main/java/it/uniroma3/siw/repository/ProviderRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Provider;
+
+public interface ProviderRepository extends CrudRepository<Provider,Long>{
+
+}
